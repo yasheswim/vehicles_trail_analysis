53946c94f21df3165b39c45b4161b8d891c077f1
Yasheswi Anirudh Mishra <74765745+yasheswim@users.noreply.github.com>
2023-06-25 16:05:05 +0530

    Update hard_coded_variables.py

--- hard_coded_variables.py
+++ hard_coded_variables.py
@@ -1,3 +1,3 @@
-TRAIL_FOLDER_PATH = "datasets/trail_datasets/GJ1BV1398.csv" 
+TRAIL_FOLDER_PATH = "datasets/trail_datasets/*.csv" 
 TRIP_INFO_DATA_PATH = "datasets/Trip-Info.csv"
 EXCEL_REPORT_PATH = 'datasets/asset_report/asset_report.xlsx'
